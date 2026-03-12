4f1f300b03a4b6bd5974d14bb113444204a52d79
Olarsen1023 <odl1014@usnh.edu>
2026-03-12 19:01:43 +0000

    Lab 13: Implement argparse word counter

--- labs/Lab13/src/wordcount.py
+++ labs/Lab13/src/wordcount.py
@@ -18,13 +18,20 @@ def build_parser():
         argparse.ArgumentParser
     """
     # TODO: Create an ArgumentParser with a description
+    parser = argparse.ArgumentParser(description="Count words in a text file.")
     # TODO: Add the positional 'filename' argument
+    parser.add_argument("filename", help="The text file to analyze")
     # TODO: Add --ignore-case / -i (action="store_true")
+    parser.add_argument("--ignore-case", "-i", action="store_true", help="Lowercase all words")
     # TODO: Add --top / -t (type=int, default=None)
+    parser.add_argument("--top", "-t", type=int, default=None, help="Show top N most frequent words")
     # TODO: Add --min-length / -m (type=int, default=1)
+    parser.add_argument("--min-length", "-m", type=int, default=1, help="Only count words with at least this many characters")
     # TODO: Add --sort-by / -s (choices=["freq", "alpha"], default="freq")
+    parser.add_argument("--sort-by", "-s", choices=["freq", "alpha"], default="freq", help="How to sort top words")
     # TODO: Add --reverse / -r (action="store_true")
-    pass
+    parser.add_argument("--reverse", "-r", action="store_true", help="Reverse the sort order")
+    return parser
 
 
 def analyze(filepath, ignore_case=False, top=None, min_length=1,
@@ -46,18 +53,32 @@ def analyze(filepath, ignore_case=False, top=None, min_length=1,
         FileNotFoundError: if the file doesn't exist
     """
     # TODO: Read the file and split into words on whitespace
+    with open(filepath, "r") as f:
+        text = f.read()
+    words = text.split()
     # TODO: If ignore_case, lowercase all words
+    if ignore_case:
+        words = [w.lower() for w in words]
     # TODO: Filter out words shorter than min_length
+    words = [w for w in words if len(w) >= min_length]
     # TODO: Count total words
+    total_words = len(words)
     # TODO: If top is None, return "<filename>: <count> words"
-    # TODO: If top is set, find the most frequent words:
-    #   - Use Counter(words).most_common() for frequency data
-    #   - If sort_by == "alpha", sort alphabetically instead
-    #   - If reverse, flip the order
-    #   - Take the first 'top' entries
-    #   - Return multi-line string:
-    #       "<filename>: <count> words\n\nTop <N> words:\n  <word>: <count>\n  ..."
-    pass
+    if top is None:
+        return f"{filepath}: {total_words} words"
+    word_counts = Counter(words)
+    if sort_by == "alpha":
+        sorted_words = sorted(word_counts.items(), key=lambda x: x[0], reverse=reverse)
+    else:
+        sorted_words = word_counts.most_common()
+    if reverse:
+        sorted_words = list(reversed(sorted_words))
+    top_words = sorted_words[:top]
+    result_lines = [f"{filepath}: {total_words} words", "", "Top 5 words:"]
+    for word, count in top_words:
+        result_lines.append(f"  {word}: {count}")
+    return "\n".join(result_lines)
+
 
 
 def main():
@@ -66,7 +87,12 @@ def main():
     # TODO: Parse args
     # TODO: Call analyze with the parsed arguments
     # TODO: Print the result
-    pass
+    parser = build_parser()
+    args = parser.parse_args()
+    result = analyze(args.filename, ignore_case=args.ignore_case, top=args.top,
+                        min_length=args.min_length, sort_by=args.sort_by, reverse=args.reverse)
+    print(result)
+
 
 
 if __name__ == "__main__":
